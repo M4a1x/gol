d179ead294d00ae3fadd9af35ae277dc48f24648
Max Stabel <M4a1x@users.noreply.github.com>
2021-03-23 13:39:43 +0100

    Add Life 1.06 parser

---
+++ src/parser/life.rs
@@ -0,0 +1,195 @@
+use std::io::{self, BufRead, BufReader, Read};
+use std::iter::Peekable;
+use std::str::FromStr;
+
+use crate::parser::{Cell, CellStatus, ParseError, Pattern, PatternConfig, Point, Rules, Size};
+
+pub fn parse(input: impl Read) -> Result<Pattern, ParseError> {
+    let buf_reader = BufReader::new(input);
+    let mut content_line_iter = buf_reader
+        .lines()
+        .filter(|l| {
+            if let Ok(line) = l.as_ref() {
+                !line.is_empty()
+            } else {
+                false
+            }
+        })
+        .map(|l| {
+            if let Ok(line) = l.as_ref() {
+                Ok(line.trim().to_owned())
+            } else {
+                l
+            }
+        })
+        .peekable();
+
+    let version = parse_version(&mut content_line_iter)?;
+    let config = parse_header(&mut content_line_iter)?;
+
+    let alive_list = match version.as_ref() {
+        "1.05" => parse_life_105_cell_blocks(&mut content_line_iter)?,
+        "1.06" => parse_life_106_list(&mut content_line_iter)?,
+        _ => {
+            return Err(ParseError::InvalidFormat(
+                "Unknown .life/.lif version".to_owned(),
+            ))
+        }
+    };
+
+    // TODO: Calculate size from alive_list and adjust list to center on top left.
+    let size = Size {
+        width: 0,
+        height: 0,
+    };
+
+    Ok(Pattern {
+        size,
+        alive_list,
+        config,
+    })
+}
+
+fn parse_version(
+    input: &mut impl Iterator<Item = io::Result<String>>,
+) -> Result<String, ParseError> {
+    if let Some(line) = input.next() {
+        let line = line?;
+        if &line[..5] != "#Life" {
+            Err(ParseError::InvalidFormat(
+                "Invalid .life/.lif file format".to_owned(),
+            ))
+        } else {
+            Ok(line[6..].to_owned())
+        }
+    } else {
+        Err(ParseError::Empty)
+    }
+}
+
+fn parse_header(
+    input: &mut Peekable<impl Iterator<Item = io::Result<String>>>,
+) -> Result<PatternConfig, ParseError> {
+    let mut description: Option<String> = None;
+    let mut ruleset = None;
+    let mut author = None;
+    let wrap_edges = false;
+
+    // Fails to parse last header line, if file has no content
+    while let Some(next_line) = input.peek() {
+        if let Ok(next_line_str) = next_line {
+            // TODO: Handle empty lines
+            if &next_line_str[..1] != "#" || &next_line_str[..2] == "#P" {
+                return Ok(PatternConfig {
+                    description,
+                    ruleset,
+                    author,
+                    wrap_edges,
+                });
+            }
+        }
+
+        if let Some(line) = input.next() {
+            let line = line?;
+
+            if line.is_empty() {
+                continue;
+            }
+
+            match &line[..2] {
+                "#C" | "#D" => {
+                    if description.is_none() {
+                        description = Some(String::new());
+                    }
+                    description = description.map(|desc| desc + line[2..].trim() + "\n");
+                }
+                "#N" => ruleset = Some(Rules::default()),
+                "#R" => ruleset = Some(Rules::from_str(&line[2..].trim())?),
+                "#O" => author = Some(line[2..].to_owned()),
+                _ => break,
+            }
+        }
+    }
+
+    Ok(PatternConfig {
+        ruleset,
+        description,
+        author,
+        wrap_edges,
+    })
+}
+
+fn parse_life_105_cell_blocks(
+    input: impl Iterator<Item = io::Result<String>>,
+) -> Result<Vec<Cell>, ParseError> {
+    panic!("Not implemented!");
+}
+
+fn parse_life_106_list(
+    input: impl Iterator<Item = io::Result<String>>,
+) -> Result<Vec<Cell>, ParseError> {
+    input
+        .map(|line| {
+            let line = line?;
+            let mut coords = line.split(' ');
+            let x: u32 = match coords.next() {
+                Some(x) => x.parse()?,
+                None => {
+                    return Err(ParseError::InvalidFormat(format!(
+                        "{}: Line contains invalid x position format",
+                        line
+                    )))
+                }
+            };
+            let y: u32 = match coords.next() {
+                Some(y) => y.parse()?,
+                None => {
+                    return Err(ParseError::InvalidFormat(format!(
+                        "{}: Line contains invalid y position format",
+                        line
+                    )))
+                }
+            };
+
+            Ok(Cell::new_alive(x, y))
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_life_106_gosper_glider_gun() {
+        let input = "#Life 1.06
+0 0
+0 1
+1 0
+
+1 2
+2 0"
+        .as_bytes();
+        let output = Pattern {
+            alive_list: vec![
+                Cell::new_alive(0, 0),
+                Cell::new_alive(0, 1),
+                Cell::new_alive(1, 0),
+                Cell::new_alive(1, 2),
+                Cell::new_alive(2, 0),
+            ],
+            config: PatternConfig {
+                author: None,
+                description: None,
+                ruleset: None,
+                wrap_edges: false,
+            },
+            size: Size {
+                height: 0,
+                width: 0,
+            },
+        };
+
+        assert_eq!(parse(input).unwrap(), output);
+    }
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,5 +1,87 @@
-pub mod parse_error;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::path::Path;
+
+pub mod error;
+pub mod life;
 pub mod rules;
 
-pub use parse_error::ParseError;
+pub use error::ParseError;
 pub use rules::Rules;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Cell {
+    status: CellStatus,
+    pos: Point,
+}
+
+impl Cell {
+    pub fn new(pos: Point, status: CellStatus) -> Self {
+        Cell { pos, status }
+    }
+
+    pub fn new_alive(x: u32, y: u32) -> Self {
+        Cell {
+            pos: Point::new(x, y),
+            status: CellStatus::Alive,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum CellStatus {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
+}
+
+impl Point {
+    pub fn new(x: u32, y: u32) -> Self {
+        Point { x, y }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Pattern {
+    size: Size,
+    alive_list: Vec<Cell>,
+    config: PatternConfig,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Size {
+    pub width: usize,
+    pub height: usize,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PatternConfig {
+    ruleset: Option<Rules>,
+    description: Option<String>,
+    author: Option<String>,
+    wrap_edges: bool,
+}
+
+pub fn load_file(filepath: &Path) -> Result<Pattern, ParseError> {
+    match filepath
+        .extension()
+        .and_then(OsStr::to_str)
+        .map(|ext| ext.to_lowercase())
+        .as_deref()
+    {
+        Some("life") | Some("lif") => life::parse(File::open(filepath)?),
+        Some("rle") => panic!("Not yet implemented!"),
+        Some("l") => panic!("Not yet implemented!"),
+        Some("plf") => panic!("Not yet implemented!"),
+        Some("mcl") => panic!("Not yet implemented!"),
+        Some(all) => Err(ParseError::UnknownFileExtension(all.to_owned())),
+        None => Err(ParseError::UnknownFileExtension(
+            "file has no extension".to_owned(),
+        )),
+    }
+}
